3a6959ad91cd4e740bc3110cd4990ab28660ab3a
Ilya <ilya.melnikov@indusoft.ru>
2016-06-29 13:47:42 +0300

    initial

--- Solution materials/set_masterpage.ps1
+++ Solution materials/set_masterpage.ps1
@@ -3,7 +3,9 @@ if ((Get-PSSnapin | ? { $_.Name -eq "Microsoft.SharePoint.PowerShell" }) -eq $nu
 }
 
 $masterPageType = Read-Host "Set default MasterPage(1) or bootstrap MasterPage(2): "
-$site = Get-SPweb "http://localhost"
+$siteUrl = Read-Host "Site url (default http://localhost): "
+if ($siteUrl -eq "") {$siteUrl = "http://localhost"}
+$site = Get-SPweb $siteUrl
 switch ($masterPageType) {
 		##default
 		"1" {
